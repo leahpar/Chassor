d0ed72075f3afe03ddf7c8a696e665ae6a6d4b28
Raphael Bacco <raphael@bacco.fr>
2013-05-20 23:30:23 +0200

    Ajout indices

---
+++ src/Raf/ChassorCoreBundle/Form/IndiceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Raf\ChassorCoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class IndiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('enigme', 'entity',   array('empty_value' => '',
+                                              'class' => 'ChassorCoreBundle:Enigme',
+                                              'property' => 'code',
+                                              'attr' => array('class' => 'myselect2')))
+            ->add('indice', 'text')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Raf\ChassorCoreBundle\Entity\Indice'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'raf_chassorcorebundle_indicetype';
+    }
+}
