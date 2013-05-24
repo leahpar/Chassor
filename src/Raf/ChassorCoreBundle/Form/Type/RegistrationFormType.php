1c04efe7bf3a987ffa71eb1de0a5aa191cd35932
leahpar <leahpar@gmail.com>
2013-05-25 00:23:48 +0200

    reorg bondles

---
+++ src/Raf/ChassorCoreBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Raf\ChassorCoreBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('nom',     'text')
+                ->add('prenom',  'text')
+                ->add('adresse', 'textarea')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'chassor_user_registration';
+    }
+}
