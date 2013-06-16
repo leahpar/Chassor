e639dd03f9685be605f8818e6df6c7de4168bf82
leahpar <leahpar@gmail.com>
2013-06-16 22:03:10 +0200

    dev++

--- src/Raf/ChassorCoreBundle/Resources/public/js/main.js
+++ src/Raf/ChassorCoreBundle/Resources/public/js/main.js
@@ -1,1 +1,11 @@
+// Lien dynamique d'inscription
+var _href = $('#lienInscription').attr('href'); 
+$('input[id=codePromo]').change(function()
+{ 
+    $('#lienInscription').attr('href', _href + '-' + $('#codePromo').val().substr(0,6))
+});
 
+// validation formulaire inscription
+// $(".fos_user_registration_register .submit").click(function(){
+// 	$(".fos_user_registration_register").submit();
+// });
