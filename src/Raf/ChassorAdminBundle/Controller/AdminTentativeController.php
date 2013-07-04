23c2b993b66a5380d7657dce8ff615e97e3238f7
leahpar <leahpar@gmail.com>
2013-07-04 09:10:09 +0200

    gestion tentatives par enigme

--- src/Raf/ChassorAdminBundle/Controller/AdminTentativeController.php
+++ src/Raf/ChassorAdminBundle/Controller/AdminTentativeController.php
@@ -4,6 +4,8 @@ namespace Raf\ChassorAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Raf\ChassorCoreBundle\Entity\Enigme;
+
 class AdminTentativeController extends Controller
 {
     public function listerAction()
@@ -12,6 +14,19 @@ class AdminTentativeController extends Controller
                       ->getManager()
                       ->getRepository('ChassorCoreBundle:Tentative')
                       ->findBy(array(), array('date' => 'DESC'));
+       
+        return $this->render('ChassorAdminBundle:Tentative:lister.html.twig',
+                array(
+                        'liste' => $liste
+                ));
+    }
+    
+    public function listerEnigmeAction(Enigme $enigme)
+    {
+        $liste = $this->getDoctrine()
+                      ->getManager()
+                      ->getRepository('ChassorCoreBundle:Tentative')
+                      ->findBy(array('enigme' => $enigme), array('date' => 'DESC'));
        
         return $this->render('ChassorAdminBundle:Tentative:lister.html.twig',
                 array(
